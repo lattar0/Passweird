258d15f976a2e4c36cb66e7eead7b93395a72774
João Lattaro <lattaro@tuta.io>
2026-03-20 20:36:58 -0300

    chore: initial setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
---
+++ app/src/main/java/com/lattaro/passweird/PasswordApplication.kt
@@ -0,0 +1,7 @@
+package com.lattaro.passweird
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PasswordApplication : Application()
