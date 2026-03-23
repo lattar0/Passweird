56139e07b96c85755ee889f6667ae37a4f026626
João Lattaro <lattaro@tuta.io>
2026-03-22 21:03:35 -0300

    feat: add PasswordFormScreen UI and ViewModel logic

--- app/src/main/java/com/lattaro/passweird/presentation/form/PasswordFormScreen.kt
+++ app/src/main/java/com/lattaro/passweird/presentation/form/PasswordFormScreen.kt
@@ -1,27 +1,43 @@
 package com.lattaro.passweird.presentation.form
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.lattaro.passweird.R
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 fun PasswordFormScreen(
-    onNavigateToPasswordList: () -> Unit
-) {
+    onNavigateToPasswordList: () -> Unit,
+    viewModel: PasswordFormViewModel = hiltViewModel(),
+    ) {
+
+    val description by viewModel.description.collectAsState()
+    val user by viewModel.user.collectAsState()
+    val password by viewModel.password.collectAsState()
+
     Scaffold(
         topBar =
             {
@@ -49,10 +65,49 @@ fun PasswordFormScreen(
                 )
             },
     ) { innerPadding ->
-        Text(
-            text = "Usuário",
-            modifier = Modifier.padding(innerPadding)
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxWidth()
+                .padding(32.dp, 16.dp)
+
+        ) {
+            OutlinedTextField(
+                value = "",
+                label = { Text("Descrição") },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                onValueChange = {
+                    viewModel.onDescriptionInputChange(it)
+                }
+            )
+
+            OutlinedTextField(
+                state = rememberTextFieldState(),
+                label = { Text("Usuário") },
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+
+            OutlinedTextField(
+                state = rememberTextFieldState(),
+                label = { Text("Senha") },
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 16.dp)
+            ) {
+                Text("Salvar senha")
+            }
+        }
+
     }
 }
 
--- app/src/main/java/com/lattaro/passweird/presentation/form/PasswordFormViewModel.kt
+++ app/src/main/java/com/lattaro/passweird/presentation/form/PasswordFormViewModel.kt
@@ -4,12 +4,35 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.lattaro.passweird.domain.model.Password
 import com.lattaro.passweird.domain.usecase.InsertPasswordUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class PasswordFormViewModel @Inject constructor(
     private val insertPasswordUseCase: InsertPasswordUseCase
 ) : ViewModel() {
+    private val _description = MutableStateFlow("")
+    val description: StateFlow<String> = _description
+
+    private val _user = MutableStateFlow("")
+    val user: StateFlow<String> = _user
+
+    private val _password = MutableStateFlow("")
+    val password: StateFlow<String> = _password
+
+    fun onPasswordInputChange(newValue: String) {
+        _password.value = newValue
+    }
+
+    fun onUserInputChange(newValue: String) {
+        _user.value = newValue
+    }
+
+    fun onDescriptionInputChange(newValue: String) {
+        _description.value = newValue
+    }
+
     fun createPassword(password: Password) {
         viewModelScope.launch {
             insertPasswordUseCase(password)
